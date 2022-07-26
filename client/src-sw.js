5a2a3e4755bec25cbbd1d1b3ec97dd22023bb709
hasnainkhalfan <73541651+hasnainkhalfan@users.noreply.github.com>
2022-07-26 09:46:50 -0400

    installed pwa, click event handlers

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,13 +28,13 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  // callback function that will filter the requests we want to cache (in this case, JS and CSS files)
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
     // cache storage name.
     cacheName: 'asset-cache',
     plugins: [
-      // This will cache responses with these headers to a maximum-age of 30 days
+      // cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
